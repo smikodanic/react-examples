ef895adbe0f0de5630de3b05c75fbaba86fadf86
Sasa M <smikodanic@gmail.com>
2024-12-08 16:11:52 +0100

    login/logout - navigate

--- src/pages/ExampleRoutingLogin.tsx
+++ src/pages/ExampleRoutingLogin.tsx
@@ -1,15 +1,20 @@
+import { useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../react/context/AuthContext';
 
 
 const ExampleRoutingLogin = () => {
-  const { login, logout } = useAuth();
+  const { isAuthenticated, login } = useAuth();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) navigate('/example-routing-dashboard');
+  }, [])
 
   const handleLogin = () => {
     console.log('LOGIN');
     login();
-    // navigate('/example-routing-dashboard');
+    navigate('/example-routing-dashboard');
   };
 
   return (
